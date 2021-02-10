f13bbde07ea84e9197b8ff48b94e4e02039397c4
Dylan <dylan.sardi@gmail.com>
2021-02-10 18:15:51 +0100

    Implémentation de la fonction operate pour gerer tout les types de calculs créés précédement. (non testé)

--- calc.js
+++ calc.js
@@ -13,3 +13,23 @@ function multiply(x, y){
 function divide(x, y){
     return(x / y);
 }
+
+function operate(operator, x , y) {
+    let resultat;
+    switch (operator) {
+        case '+':
+            resultat = add(x, y);
+            break;
+        case '-':
+            resultat = substract(x, y);
+            break;
+        case '*':
+            resultat = multiply(x, y);
+            break;
+        case '/':
+            resultat = divide(x, y);
+            break;
+    }
+    return resultat;
+
+}
