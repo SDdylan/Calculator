94d0607c5199a991f33ecdb91c5f3618cc0a9cd1
Dylan <dylan.sardi@gmail.com>
2021-02-12 18:21:48 +0100

    Calculs à 2 nombres fonctionnels mais seulement pour les entiers Modification légère du CSS Ajout des fonctionnalités de reset et de calcul aux boutons

--- calc.js
+++ calc.js
@@ -14,7 +14,7 @@ function divide(x, y){
     return(x / y);
 }
 
-function operate(operator, x , y) {
+function operate(x, operator , y) {
     let resultat;
     switch (operator) {
         case '+':
@@ -36,10 +36,35 @@ function operate(operator, x , y) {
 
 }
 
+
+console.log(add(12,8));
+alert(add(12, 8));
+
+//stockage de la value du calcul pour plus tard
+let calcul ;
+
 //fonction pour ajouter la valeur du bouton a l'écran de texte de la calculatrice
 function numberScreen(id) {
-    var text = toString(document.getElementById(id).textContent);
-    document.getElementById('tap').textContent += text;
+    let text = document.getElementById(id).textContent;
+    console.log(text);
+    if (text === '+' || text === '-' || text === '*' || text === '/') {
+        document.getElementById('screen').value += " " + text + " ";
+    } else {
+        document.getElementById('screen').value += text;
+    }
 }
 
-console.log(operate('*',6,3));
+//fonction pour calculer (stocker dans un premier lieu)
+function egalScreen() {
+    calcul = document.getElementById('screen').value;
+    let elements = calcul.split(" ");
+    alert(operate(parseFloat(elements[0]),elements[1],parseFloat(elements[2])))
+    console.log(elements);
+}
+
+
+function resetScreen() {
+    document.getElementById('screen').value = '';
+}
+
+console.log(operate(6,'+',3));
